aed8e95f566c746dd1c0885459d3f18923cedc49
Simon Maddox <simon@simonmaddox.com>
2013-08-31 13:55:55 +0100

    Initial Commit

---
+++ Hearty/SMXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SMXAppDelegate.h
+//  Hearty
+//
+//  Created by Simon Maddox on 31/08/2013.
+//  Copyright (c) 2013 Simon Maddox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SMXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
