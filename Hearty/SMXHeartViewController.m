682bf15367268c9a442fa613f5743ec7d5813fec
Simon Maddox <simon@simonmaddox.com>
2013-09-01 18:07:57 +0100

    Animate a heart beat

--- Hearty/SMXHeartViewController.m
+++ Hearty/SMXHeartViewController.m
@@ -8,9 +8,13 @@
 
 #import "SMXHeartViewController.h"
 #import "SMXHeartRateMonitor.h"
+#import <iOS-blur/AMBlurView.h>
 
 @interface SMXHeartViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *label;
+@property (weak, nonatomic) IBOutlet UIImageView *heartView;
+@property (weak, nonatomic) IBOutlet UILabel *bpmLabel;
+@property (weak, nonatomic) IBOutlet AMBlurView *blurView;
 
 @end
 
@@ -23,10 +27,46 @@
     @weakify(self);
 	SMXHeartRateMonitorUpdated updatedBlock = ^(NSInteger heartRate){
         @strongify(self);
+        
+        @weakify(self);
+        
+        if (self.blurView){
+            
+            [UIView animateWithDuration:0.5 animations:^{
+                @strongify(self);
+                self.blurView.alpha = 0;
+            } completion:^(BOOL finished) {
+                @strongify(self);
+                [self.blurView removeFromSuperview];
+                self.blurView = nil;
+            }];
+        }
+    
         self.label.text = [NSString stringWithFormat:@"%d", heartRate];
-        //NSLog(@"%d BPM", heartRate);
     };
     [[SMXHeartRateMonitor sharedInstance] addHeartRateUpdatedCallback:updatedBlock];
+    
+    [self animateHeartBeat];
+}
+
+- (void) animateHeartBeat
+{
+    // TODO: Animate this at the same rate as the user's heart
+    @weakify(self);
+    [UIView animateWithDuration:60.0 / 120.0
+                          delay:0
+         usingSpringWithDamping:0.5
+          initialSpringVelocity:1
+                        options:UIViewAnimationOptionAutoreverse | UIViewAnimationOptionRepeat
+                     animations:^{
+                         
+                         @strongify(self);
+                         CGFloat scaleFactor = 0.9;
+                         self.heartView.transform = CGAffineTransformScale(self.heartView.transform, scaleFactor, scaleFactor);
+                         
+                     } completion:^(BOOL finished) {
+
+                     }];
 }
 
 @end
--- Podfile
+++ Podfile
@@ -1,2 +1,3 @@
 platform :ios, '7.0'
 pod 'libextobjc', '~> 0.3'
+pod 'iOS-blur', '~> 0.0.2'
